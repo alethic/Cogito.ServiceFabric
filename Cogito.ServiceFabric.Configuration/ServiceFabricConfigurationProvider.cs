72cb88ec6c778230812655eb8f95046b7ec91d02
Jerome Haltom <wasabi@cogito.cx>
2018-07-10 09:01:28 -0500

    Import FileAndServe.ServiceFabric.Configuration.

---
+++ Cogito.ServiceFabric.Configuration/ServiceFabricConfigurationProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Fabric;
+using System.Net;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Cogito.ServiceFabric.Configuration
+{
+
+    /// <summary>
+    /// Provides a given package name as configuration.
+    /// </summary>
+    public class ServiceFabricConfigurationProvider :
+        ConfigurationProvider
+    {
+
+        readonly string packageName;
+        readonly CodePackageActivationContext context;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="packageName"></param>
+        public ServiceFabricConfigurationProvider(string packageName)
+        {
+            this.packageName = packageName;
+
+            context = FabricRuntime.GetActivationContext();
+            context.ConfigurationPackageModifiedEvent += (sender, e) =>
+            {
+                LoadPackage(e.NewPackage, reload: true);
+                OnReload();
+            };
+        }
+
+        public override void Load()
+        {
+            LoadPackage(context.GetConfigurationPackageObject(packageName));
+        }
+
+        void LoadPackage(ConfigurationPackage config, bool reload = false)
+        {
+            if (reload)
+                Data.Clear();
+
+            foreach (var section in config.Settings.Sections)
+                foreach (var param in section.Parameters)
+                    Data[$"{section.Name}:{param.Name}"] = param.IsEncrypted ? new NetworkCredential("", param.DecryptValue()).Password : param.Value;
+        }
+
+    }
+
+}
---
+++ Cogito.ServiceFabric.Configuration/ServiceFabricConfigurationSource.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Cogito.ServiceFabric.Configuration
+{
+
+    /// <summary>
+    /// Provides sources of configuration information from a given package name.
+    /// </summary>
+    class ServiceFabricConfigurationSource :
+        IConfigurationSource
+    {
+
+        readonly string packageName;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="packageName"></param>
+        public ServiceFabricConfigurationSource(string packageName)
+        {
+            this.packageName = packageName;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new ServiceFabricConfigurationProvider(packageName);
+        }
+
+    }
+
+}
