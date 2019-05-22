9081d3937b1eb808d2b29c41ff2450edad4ca9f2
Jerome Haltom <wasabi@cogito.cx>
2019-05-22 02:14:00 -0500

    Add module for pulling in configuration.

---
+++ Cogito.ServiceFabric.Configuration.Autofac/ServiceFabricConfigurationBuilderConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Fabric;
+
+using Cogito.Autofac;
+using Cogito.Extensions.Configuration.Autofac;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Cogito.ServiceFabric.Configuration.Autofac
+{
+
+    /// <summary>
+    /// Applies available configuration packages to the configuration framework.
+    /// </summary>
+    [RegisterAs(typeof(IConfigurationBuilderConfiguration))]
+    public class ServiceFabricConfigurationBuilderConfiguration :
+        IConfigurationBuilderConfiguration
+    {
+
+        public IConfigurationBuilder Apply(IConfigurationBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            var context = FabricRuntime.GetActivationContext();
+            if (context != null)
+                foreach (var config in context.GetConfigurationPackageNames())
+                    builder = builder.AddServiceFabricConfig(config);
+
+            return builder;
+        }
+
+    }
+
+}
---
+++ Cogito.ServiceFabric.Configuration.Autofac/ServiceFabricModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+using Cogito.Autofac;
+
+namespace Cogito.ServiceFabric.Configuration.Autofac
+{
+
+    public class ServiceFabricModule : ModuleBase
+    {
+
+        protected override void Register(ContainerBuilder builder)
+        {
+            if (FabricEnvironment.IsFabric)
+                builder.RegisterFromAttributes(typeof(ServiceFabricModule).Assembly);
+        }
+
+    }
+
+}
