c5ab33daf4f63227a50da0a71afbfb350d28630c
Jerome Haltom <jhaltom@alethic.solutions>
2020-10-28 12:54:42 -0500

    Add dependencies.

--- Cogito.ServiceFabric.Test.AspNetCore.WebService.Net47/AssemblyModule.cs
+++ Cogito.ServiceFabric.Test.AspNetCore.WebService.Net47/AssemblyModule.cs
@@ -10,6 +10,9 @@ namespace Cogito.ServiceFabric.Test.AspNetCore.WebService.Net47
 
         protected override void Register(ContainerBuilder builder)
         {
+            builder.RegisterModule<Cogito.Extensions.Logging.Serilog.Autofac.AssemblyModule>();
+            builder.RegisterModule<Cogito.ServiceFabric.Configuration.Autofac.AssemblyModule>();
+            builder.RegisterModule<Cogito.ServiceFabric.AspNetCore.Kestrel.Autofac.AssemblyModule>();
             builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
         }
 
